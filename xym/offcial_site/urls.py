11ccc4cc1624b52853661a461ba1c073db7faf4f
Xunhui <17888838538@163.com>
2021-11-12 18:50:04 +0800

    new size start

--- xym/offcial_site/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from . import views
-
-
-urlpatterns = [
-    path("", view=views.home, name="home"),
-    path("api/members/", view=views.members, name="member_list"),
-]
